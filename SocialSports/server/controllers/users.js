46cf260f8a2ba0656148a0d35281081130f7afb6
Uzair Vawda <uzair.vawda@gmail.com>
2023-03-18 12:49:00 -0400

    setting up backend routes, controllers and verification middleware

---
+++ SocialSports/server/controllers/users.js
@@ -0,0 +1,70 @@
+import User from "../models/User.js";
+
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    // loop over users friend list to get each user details
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+
+    const formattedFriendsList = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriendsList);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+
+    await user.save();
+    await friend.save();
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+
+    const formattedFriendsList = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return {
+          _id,
+          firstName,
+          lastName,
+          occupation,
+          location,
+          picturePath,
+        };
+      }
+    );
+    res.status(200).json(formattedFriendsList);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};

6a9d86a407da5a9267e020f133a1aafcaf887c96
Uzair Vawda <uzair.vawda@gmail.com>
2023-04-02 17:06:38 -0400

    adding login/registration functionality

--- SocialMedia/client/src/scenes/loginPage/Form.jsx
+++ SocialMedia/client/src/scenes/loginPage/Form.jsx
@@ -55,7 +55,46 @@ export default function Form() {
   const isLogin = pageType === "login";
   const isRegister = pageType === "register";
 
-  const handleFormSubmit = async (values, onSubmitProps) => {};
+  const register = async (values, onSubmitProps) => {
+    const formData = new FormData();
+    for (let value in values) {
+      formData.append(value, values[value]);
+    }
+    formData.append("picturePath", values.picture.name);
+    const savedUserRes = await fetch("http://localhost:3001/auth/register", {
+      method: "POST",
+      body: formData,
+    });
+    const savedUser = await savedUserRes.json();
+    onSubmitProps.resetForm();
+
+    if (savedUser) {
+      setPageType("login");
+    }
+    console.log(savedUser);
+  };
+
+  const login = async (values, onSubmitProps) => {
+    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    });
+
+    const loggedIn = await loggedInResponse.json();
+    onSubmitProps.resetForm();
+
+    if (loggedIn) {
+      dispatch(setLogin({ user: loggedIn.user, token: loggedIn.token }));
+      navigate("/home");
+    }
+    console.log(loggedIn);
+  };
+
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    if (isLogin) await login(values, onSubmitProps);
+    if (isRegister) await register(values, onSubmitProps);
+  };
 
   return (
     <Formik
@@ -186,6 +225,40 @@ export default function Form() {
               helperText={touched.password && errors.password}
               sx={{ gridColumn: "span 4" }}
             />
+          </Box>
+
+          <Box>
+            <Button
+              fullWidth
+              type="submit"
+              sx={{
+                m: "2rem 0",
+                p: "1rem",
+                backgroundColor: palette.primary.main,
+                color: palette.background.alt,
+                "&:hover": { color: palette.primary.main },
+              }}
+            >
+              {isLogin ? "LOGIN" : "REGISTER"}
+            </Button>
+            <Typography
+              onClick={() => {
+                setPageType(isLogin ? "register" : "login");
+                resetForm();
+              }}
+              sx={{
+                textDecoration: "underline",
+                color: palette.primary.main,
+                "&:hover": {
+                  cursor: "pointer",
+                  color: palette.primary.light,
+                },
+              }}
+            >
+              {isLogin
+                ? "Don't have an account? Sign Up here!"
+                : "Have an account? Login here!"}
+            </Typography>
           </Box>
         </form>
       )}

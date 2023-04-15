cf7726b51f44f4bac7d9534d8430686ea2b26364
Uzair Vawda <uzair.vawda@gmail.com>
2023-04-15 15:13:48 -0400

    user widget with apui call

--- SocialMedia/client/src/scenes/homePage/index.jsx
+++ SocialMedia/client/src/scenes/homePage/index.jsx
@@ -1,11 +1,33 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const HomePage = () => {
+  const isNotMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNotMobileScreens ? "flex" : "block"}
+      >
+        <Box flexBasis={isNotMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNotMobileScreens ? "42%" : undefined}
+          mt={isNotMobileScreens ? undefined : "2rem"}
+        >
+          {/* WIDGETS */}
+        </Box>
+
+        {isNotMobileScreens && <Box flexBasis="26%"></Box>}
+      </Box>
     </Box>
   );
 };
